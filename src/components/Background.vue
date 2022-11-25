2e7563ca2235bf4a150c23cd201854f8ca662880
Semi-dios <tipouno@misena.edu.co>
2022-11-25 13:30:17 -0500

    feat-homescreen/carousel_styles

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,6 +1,23 @@
 <template>
   <section class="section">
-    <div class="container">
+    <div class="container bg-dark__carousel is-fluid">
+      <div class="bg-dark__carousel-social-media">
+        <a href="#" target="_blank" title="linkedin">
+          <span class="icon is-medium">
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </span>
+        </a>
+        <a href="#" target="_blank" title="github">
+          <span class="icon is-medium">
+            <ion-icon name="logo-github"></ion-icon>
+          </span>
+        </a>
+        <a href="#" target="_blank" title="youtube">
+          <span class="icon is-medium">
+            <ion-icon name="logo-youtube"></ion-icon>
+          </span>
+        </a>
+      </div>
       <header>
         <slot name="header"></slot>
       </header>
@@ -10,6 +27,20 @@
       <footer>
         <slot name="footer"></slot>
       </footer>
+      <div class="bg-dark__carousel-steps">
+        <span class="icon bg-dark__carousel-steps-item item-seleted">
+          <ion-icon name="ellipse-sharp"></ion-icon>
+        </span>
+        <span class="icon bg-dark__carousel-steps-item">
+          <ion-icon name="ellipse-sharp"></ion-icon>
+        </span>
+        <span class="icon bg-dark__carousel-steps-item">
+          <ion-icon name="ellipse-sharp"></ion-icon>
+        </span>
+        <span class="icon bg-dark__carousel-steps-item">
+          <ion-icon name="ellipse-sharp"></ion-icon>
+        </span>
+      </div>
     </div>
   </section>
 </template>
---
+++ src/steps/HomeStep.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="columns is-vcentered">
+    <div class="column is-variable is-12-mobile is-6-tablet">First column</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { mapState } from "vuex";
+export default defineComponent({
+  name: "HomeStep",
+  components: {},
+  computed: {
+    ...mapState(["backgroundDark"]),
+  },
+});
+</script>
